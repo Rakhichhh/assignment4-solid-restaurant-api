1a72423d7fce988963e4ae1a005f30bd5e382cff
Rakhichhh <rakhat.myrzabekov@bk.ru>
2026-02-01 14:26:37 +0500

    add project layered architecture

--- src/Main.java
+++ src/Main.java
@@ -16,6 +16,7 @@ public class Main {
         for (BaseEntity e : entities) {
             System.out.println(e.getType() + " -> " + e.toDisplayString());
         }
+
         Order order = new Order(1, "Order#1", "NEW");
         order.addItem(new OrderItem(1, pizza, 2));
         order.addItem(new OrderItem(2, cola, 1));
