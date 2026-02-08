cc321176faa72cc0203f1815e7e43ec394e15fb5
Rakhichhh <xw5q8fdgv9@privaterelay.appleid.com>
2026-02-08 16:46:15 +0500

    Update BaseEntity.java

--- src/model/BaseEntity.java
+++ src/model/BaseEntity.java
@@ -1,6 +1,7 @@
 package model;
 
 public abstract class BaseEntity {
+
     protected int id;
     protected String name;
 
@@ -9,19 +10,35 @@ public abstract class BaseEntity {
         this.name = name;
     }
 
-    // два абстрактных метода
+   
     public abstract String getType();
     public abstract String toDisplayString();
 
-    //1 конкретный метод
+
     public String shortInfo() {
         return id + " | " + name;
     }
 
-    //инкапсуляция
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
+  
+    public int getId() {
+        return id;
+    }
 
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    @Override
+    public String toString() {
+        return toDisplayString();
+    }
 }
