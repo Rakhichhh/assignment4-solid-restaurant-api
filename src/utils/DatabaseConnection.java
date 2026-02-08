fda82714676d7bd81cdd8f7da3e1fb65465c15d2
Rakhichhh <xw5q8fdgv9@privaterelay.appleid.com>
2026-02-08 16:39:37 +0500

    Update DatabaseConnection.java

--- src/utils/DatabaseConnection.java
+++ src/utils/DatabaseConnection.java
@@ -7,9 +7,19 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
 
+    private static DatabaseConnection instance;
     private static final String URL = "jdbc:sqlite:restaurant.db";
 
-    public static Connection getConnection() throws DatabaseOperationException {
+    private DatabaseConnection() {}
+
+    public static synchronized DatabaseConnection getInstance() {
+        if (instance == null) {
+            instance = new DatabaseConnection();
+        }
+        return instance;
+    }
+
+    public Connection getConnection() throws DatabaseOperationException {
         try {
             return DriverManager.getConnection(URL);
         } catch (SQLException e) {
